08d26a61f4ccf401f6abc480d23e7feca1587bc1
Kevin Deng <sxzz@sxzz.moe>
2026-06-30 02:41:57 +0800

    chore: fix lint

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,6 @@ import { expect, test } from 'vitest'
 import { normalizePath } from '../src'
 
 test('path', () => {
-  expect(normalizePath(String.raw`C:\foo\bar`)).toBe(String.raw`C:/foo/bar`)
+  expect(normalizePath(String.raw`C:\foo\bar`)).toBe('C:/foo/bar')
   expect(normalizePath(`/path`)).toBe('/path')
 })
